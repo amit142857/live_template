ffd96176c0d5f2f28e76792b488aea7a1be15f64
Amit <ydvamit82@gmail.com>
2021-01-05 23:35:40 +0545

    add simple dialog to ask question to user from alertbox and get answer in alertbox

--- lib/main.dart
+++ lib/main.dart
@@ -13,26 +13,47 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
-
+ enum Answers{yes, no, maybe}
 class _MyAppState extends State<MyApp> {
+  String _value = "";
+  void _setValue(String value) => setState(() => _value = value);
 
-  Future _showalert(BuildContext context, String message) async{
-    return showDialog(
+  Future _askuser() async{
+    switch(
+    await showDialog(
       context: context,
       // ignore: deprecated_member_use
-      child: AlertDialog(
-        title: Text(message),
-        scrollable: true,
-        content: Text("The battery health of your phone is very very low, please change it ASAP"),
-        actions: [
-          FlatButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text("Close alert"),
+      child: SimpleDialog(
+        title: Text("Babe do you love me?"),
+        children:<Widget> [
+          SimpleDialogOption(
+            child: Text("Yes"),
+            onPressed: () => Navigator.pop(context, Answers.yes),
+          ),
+          SimpleDialogOption(
+            child: Text("No"),
+            onPressed: () => Navigator.pop(context, Answers.no),
+          ),
+          SimpleDialogOption(
+            child: Text("Maybe"),
+            onPressed: () => Navigator.pop(context, Answers.maybe),
           )
         ],
       )
-    );
+    )
+    ){
+      case Answers.yes:
+        _setValue("Yes");
+        break;
+      case Answers.no:
+        _setValue("No");
+        break;
+      case Answers.maybe:
+        _setValue("Maybe");
+        break;
+    }
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -48,9 +69,10 @@ class _MyAppState extends State<MyApp> {
         child: Center(
           child: Column(
             children:<Widget> [
+              Text(_value),
               RaisedButton(
-                onPressed:() => _showalert(context, "Battery!!"),
-                child: Text("Show alert"),
+                onPressed: () => _askuser(),
+                child: Text('Show Simple dialog'),
               )
             ],
           ),
