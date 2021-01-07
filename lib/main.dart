3aab223e99e5f23167b0dcd7efbb2e366dbbca69
Amit <ydvamit82@gmail.com>
2021-01-07 23:18:59 +0545

    Rows and COlumns

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'dart:async';
@@ -13,48 +14,9 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
- enum Answers{yes, no, maybe}
 class _MyAppState extends State<MyApp> {
-  String _value = "";
-  void _setValue(String value) => setState(() => _value = value);
-
-  Future _askuser() async{
-    switch(
-    await showDialog(
-      context: context,
-      // ignore: deprecated_member_use
-      child: SimpleDialog(
-        title: Text("Babe do you love me?"),
-        children:<Widget> [
-          SimpleDialogOption(
-            child: Text("Yes"),
-            onPressed: () => Navigator.pop(context, Answers.yes),
-          ),
-          SimpleDialogOption(
-            child: Text("No"),
-            onPressed: () => Navigator.pop(context, Answers.no),
-          ),
-          SimpleDialogOption(
-            child: Text("Maybe"),
-            onPressed: () => Navigator.pop(context, Answers.maybe),
-          )
-        ],
-      )
-    )
-    ){
-      case Answers.yes:
-        _setValue("Yes");
-        break;
-      case Answers.no:
-        _setValue("No");
-        break;
-      case Answers.maybe:
-        _setValue("Maybe");
-        break;
-    }
-  }
-
-
+  TextEditingController _user = new TextEditingController();
+  TextEditingController _pass = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -62,18 +24,39 @@ class _MyAppState extends State<MyApp> {
         title:new Text("Name here"),
         backgroundColor: Colors.red,
       ),
-
-
       body: Container(
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
             children:<Widget> [
-              Text(_value),
-              RaisedButton(
-                onPressed: () => _askuser(),
-                child: Text('Show Simple dialog'),
-              )
+              Text("Please Login"),
+              Row(
+                children:<Widget> [
+                  Text("Username: "),
+                  Expanded(
+                    child: TextField(
+                      controller: _user,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children:<Widget> [
+                  Text("Password: "),
+                  Expanded(
+                    child: TextField(
+                      controller: _pass,
+                      obscureText: true,
+                    ),
+                  )
+                ],
+              ),
+              Padding(padding: EdgeInsets.all(12),
+              child: RaisedButton(
+                onPressed: () => print("Login ${_user.text}"),
+                child: Text("Click me"),
+              ),
+              ),
             ],
           ),
         ),
