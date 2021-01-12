d7cd732cb10d2cb68b42fad84f52dabf305694b9
Amit <ydvamit82@gmail.com>
2021-01-12 23:09:05 +0545

    chips, keys and children

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,52 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _value = "Nothing yet";
 
-  void _onPressed() => setState(() => _value = DateTime.now().toString());
+  int counter = 0;
+  List<Widget> _list = new List<Widget>();
+  @override
+  void initState() {
+    for (int i = 0; i<5; i++){
+      Widget child = _newItem(i);
+      _list.add(child);
+    }
+  }
+
+  void _onPressed() {
+    Widget child = _newItem(counter);
+    setState(() => _list.add(child));
+  }
+
+  void _removeItem(Key key){
+    for(int i = 0; i<_list.length; i++){
+      Widget child = _list.elementAt(i);
+      if (child.key == key){
+        setState(() => _list.removeAt(i));
+        print("Removing ${key.toString()}");
+      }
+    }
+  }
+
+  Widget _newItem(int i){
+    Key key = Key("Items ${i}");
+    Container child = new Container(
+      key: key,
+      padding: EdgeInsets.all(15),
+      child: Chip(
+        label: Text("This is chip number ${i}"),
+        deleteIconColor: Colors.red,
+        deleteButtonTooltipMessage: "Delete",
+        onDeleted:() => _removeItem(key),
+        avatar: CircleAvatar(
+          backgroundImage: AssetImage("Images/amit.jpg"),
+          //backgroundColor: Colors.blue.shade800,
+          //child: Text(i.toString()),
+        ),
+      ),
+    );
+    counter++;
+    return child;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,24 +66,24 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title: Text("Name Here"),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onPressed,
+        child: Icon(
+          Icons.add
+        ),
+
+      ),
       body: Container(
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children:<Widget> [
-              Text(_value),
-              IconButton(
-                icon: Icon(Icons.access_time),
-                onPressed: _onPressed,
-                color: Colors.blue,
-                iconSize: 50,
-                tooltip: "Show date and time",
-              )
-            ],
+            children: _list
           ),
         ),
       ),
     );
   }
+
+
 }
 
