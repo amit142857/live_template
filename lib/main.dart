1fa2a61d9ac7651cf26f5e36a886cd42327fe75f
Amit <ydvamit82@gmail.com>
2021-01-12 22:03:30 +0545

    listview.builder

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp(
-    items: List<String>.generate(10000, (i) => "Item $i"),
   ));
 }
 
 class MyApp extends StatelessWidget {
-  final List<String> items;
-
-  MyApp({Key key, @required this.items}) : super(key: key);
+  final items = List<String>.generate(10000, (i) => "Item $i");
 
   @override
   Widget build(BuildContext context) {
@@ -24,9 +21,13 @@ class MyApp extends StatelessWidget {
         ),
         body: ListView.builder(
           itemCount: items.length,
-          itemBuilder: (context, index) {
+          itemBuilder: (BuildContext context, index) {
             return ListTile(
               title: Text('${items[index]}'),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage("Images/amit.jpg"),
+              ),
+              trailing: Text("${index} item"),
             );
           },
         ),
