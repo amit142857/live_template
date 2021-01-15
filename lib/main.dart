147698ba360af98c2d3e4ac030129e2fb8c46bae
Amit <ydvamit82@gmail.com>
2021-01-15 21:26:35 +0545

    Slider and indicator

--- lib/main.dart
+++ lib/main.dart
@@ -13,52 +13,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  double _value = 0;
 
-  int counter = 0;
-  List<Widget> _list = new List<Widget>();
-  @override
-  void initState() {
-    for (int i = 0; i<5; i++){
-      Widget child = _newItem(i);
-      _list.add(child);
-    }
+  void _onChanged(double value) {
+    setState(() => _value = value);
   }
 
-  void _onPressed() {
-    Widget child = _newItem(counter);
-    setState(() => _list.add(child));
-  }
-
-  void _removeItem(Key key){
-    for(int i = 0; i<_list.length; i++){
-      Widget child = _list.elementAt(i);
-      if (child.key == key){
-        setState(() => _list.removeAt(i));
-        print("Removing ${key.toString()}");
-      }
-    }
-  }
-
-  Widget _newItem(int i){
-    Key key = Key("Items ${i}");
-    Container child = new Container(
-      key: key,
-      padding: EdgeInsets.all(15),
-      child: Chip(
-        label: Text("This is chip number ${i}"),
-        deleteIconColor: Colors.red,
-        deleteButtonTooltipMessage: "Delete",
-        onDeleted:() => _removeItem(key),
-        avatar: CircleAvatar(
-          backgroundImage: AssetImage("Images/amit.jpg"),
-          //backgroundColor: Colors.blue.shade800,
-          //child: Text(i.toString()),
-        ),
-      ),
-    );
-    counter++;
-    return child;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,18 +26,37 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title: Text("Name Here"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _onPressed,
-        child: Icon(
-          Icons.add
-        ),
 
-      ),
       body: Container(
         padding: EdgeInsets.all(32),
         child: Center(
           child: Column(
-            children: _list
+            children:<Widget> [
+              Text("Let us know your age"),
+              Text("${_value*100} %"),
+              Slider(
+                value: _value,
+                onChanged: _onChanged,
+                label: "Age",
+                divisions: 50,
+              ),
+              Container(
+                padding: EdgeInsets.all(32),
+                child: LinearProgressIndicator(
+                  value: _value,
+                  backgroundColor: Colors.red,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                )
+              ),
+              Container(
+                padding: EdgeInsets.all(32),
+                child: CircularProgressIndicator(
+                  value: _value,
+                  backgroundColor: Colors.white,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+                ),
+              )
+            ],
           ),
         ),
       ),
