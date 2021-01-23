4bb6c89b0996cd67b05612393c25fa54e3480528
Amit <ydvamit82@gmail.com>
2021-01-23 23:43:52 +0545

    Bar diagram

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,72 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:charts_flutter/flutter.dart' as charts;
+
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
+  runApp(new MaterialApp(
+    home: new MyApp(),
   ));
 }
 
 class MyApp extends StatefulWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  _State createState() => new _State();
 }
 
-  class Area {
-  int index;
-  Color color;
-  String name;
-
-  Area({this.index: -1, this.color: Colors.blue, this.name: "Area"});
+class Sales {
+  String year;
+  int sales;
+  Sales(this.year, this.sales);
 }
 
-class _MyAppState extends State<MyApp> {
+class _State extends State<MyApp> {
 
-  int _location;
-  List<Area> _areas;
+  List<Sales> _data;
+  List<charts.Series<Sales, String>> _chartdata;
+
+  void _makeData() {
+    _data = new List<Sales>();
+    _chartdata = new List<charts.Series<Sales, String>>();
+
+    final rnd = new Random();
+    for(int i = 2010; i < 2019; i++) {
+      _data.add(new Sales(i.toString(), rnd.nextInt(1000)));
+    }
+
+    _chartdata.add(new charts.Series(
+        id: 'Sales',
+        colorFn: (Sales sales,__) => charts.MaterialPalette.red.shadeDefault,
+        data: _data,
+        domainFn: (Sales sales, _) => sales.year,
+        measureFn: (Sales sales, _) => sales.sales,
+        fillPatternFn: (Sales sales,__) => charts.FillPatternType.forwardHatch,
+        displayName: 'sales'
+    )
+    );
+
+  }
 
 
   @override
   void initState() {
-    _areas = List<Area>();
-    for (int i = 0; i < 16; i++){
-      _areas.add(Area(
-        index: i,
-        name: "Area ${i}",
-      ));
-    }
-    var rng = Random();
-    _location = rng.nextInt(_areas.length);
-  }
-
-  Widget _generate(int index) {
-    return GridTile(
-      child: Container(
-        padding: EdgeInsets.all(5),
-        child: RaisedButton(
-          onPressed:() => _onPressed(index),
-          color: _areas[index].color,
-          child: Text(_areas[index].name, textAlign: TextAlign.center,),
-        ),
-      ),
-    );
-  }
-
-  void _onPressed(int index) {
-    setState(() {
-      if (index == _location){
-        _areas[index].color = Colors.green;
-      } else {
-        _areas[index].color = Colors.red;
-      }
-    });
-    debugPrint("$_location");
+    _makeData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Name Here"),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Name here'),
       ),
-      body: Container(
-          padding: EdgeInsets.all(32),
-          child: Center(
-            child: GridView.count(
-              crossAxisCount: 4,
-              children:
-                List<Widget>.generate(16, _generate)
+      body: new Container(
+          padding: new EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Text('Sales Data'),
+                new Expanded(child: new charts.BarChart(_chartdata))
+              ],
             ),
           )
       ),
