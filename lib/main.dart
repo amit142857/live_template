40ba2379f0a2f6c4628877633af0c8311392399f
Amit <ydvamit82@gmail.com>
2021-05-07 22:56:57 +0545

    flutter animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,58 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home(),
+  runApp(new MaterialApp(
+    home: new MyApp(),
   ));
 }
 
-class Home extends StatefulWidget {
+class MyApp extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _State createState() => new _State();
 }
 
-class _painter extends CustomPainter{
+class _State extends State<MyApp> with SingleTickerProviderStateMixin {
+
+  Animation<double> animation;
+  AnimationController controller;
+
+
   @override
-  void paint(Canvas canvas, Size size) {
-    final radius = 150.0;
-    final Offset offset = Offset(0.0 , 0.0);
-    final Paint paint = Paint()
-      ..isAntiAlias = true
-      ..strokeWidth = 15.0
-      ..color = Colors.brown[500]
-      ..style = PaintingStyle.stroke; 
-    // these .. means we are using the last object, in this case the object is Paint()
-    final Offset bodystart = Offset(0.0, 150.0);
-    final Offset bodyend = Offset(0.0, -150.0);
-    final Offset start = Offset(-150, 0);
-    final Offset end =  Offset(150 , 0);
+  void initState() {
+    super.initState();
 
-    canvas.drawCircle(offset, radius, paint);
-    canvas.drawLine(bodystart, bodyend, paint);
-    canvas.drawLine(start, end, paint);
+    controller = new AnimationController( duration: const Duration(milliseconds: 5000),vsync: this);
+    animation = new Tween(begin: 0.0, end: 200.0).animate(controller);
+    animation.addListener(() {
+      setState(() {
+        //The state of the animation has changed
+      });
+    });
 
-    final Rect rect =  new Rect.fromCircle(center: offset, radius: 40.0);
-    final Paint rectPaint = new Paint()
-      ..isAntiAlias = true
-      ..strokeWidth = 15.0
-      ..color = Colors.red
-      ..style = PaintingStyle.stroke;
-
-    canvas.drawRect(rect, rectPaint);
-
+    controller.forward();
   }
+
+
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
-}
 
-class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        centerTitle: true,
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Name here'),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(32),
-          child: Center(
-            child: Column(
-              children:<Widget> [
-                Text("Welcome to the Home" , style: TextStyle(
-                  fontSize: 30 , fontWeight:FontWeight.bold)),
-                SizedBox(
-                  height: 300,
-                ),
-                CustomPaint(
-                  painter: _painter(),
-                ),
-              ],
-            ),
-          ),
-        ),
+      body: new Container(
+          padding: new EdgeInsets.all(32.0),
+          height: animation.value,
+          width: animation.value,
+          child: new Center(
+              child: new FlutterLogo(size: 1200.0,)
+          )
       ),
     );
   }
