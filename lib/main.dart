5f30af3e603a6919eb0ee40e1ab6e93476e3a9ec
Amit <ydvamit82@gmail.com>
2021-05-10 15:25:39 +0545

    Animate the whole logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+
 
 void main() {
   runApp(new MaterialApp(
@@ -6,13 +8,39 @@ void main() {
   ));
 }
 
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> with TickerProviderStateMixin {
+class AnimatedLogo extends AnimatedWidget{
 
+  static final _opacityTween = Tween<double>(begin: 0.1 , end: 1.0);
+  static final _sizeTween = Tween<double>(begin: 0.0 , end: 300.0);
+  static final _rotateTween = Tween<double>(begin: 0.0 , end: 12.0);
+
+  AnimatedLogo({Key key, Animation<double> animation}) : super (key: key, listenable: animation);
+
+  Widget build(BuildContext context){
+    final Animation<double> animation = listenable;
+    
+    return Center(
+      child: Transform.rotate(angle: _rotateTween.evaluate(animation),
+      child: Opacity(opacity: _opacityTween.evaluate(animation),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        height: _sizeTween.evaluate(animation),
+        width: _sizeTween.evaluate(animation),
+        child: FlutterLogo(),
+      ),
+      ),
+      ),
+    );
+  }
+}
+
+class _HomeState extends State<Home> with TickerProviderStateMixin {
   Animation animation;
   AnimationController controller;
 
@@ -21,32 +49,19 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
   void initState() {
     super.initState();
 
-    controller = AnimationController(
-      duration: const Duration(milliseconds: 5000),
-      vsync: this
-    );
+    controller = AnimationController(duration: const Duration(milliseconds: 3000), vsync: this);
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
 
-    final CurvedAnimation curve = CurvedAnimation(parent: controller, curve:Curves.easeIn);
-    animation = Tween(begin: 100.0 , end: 300.0).animate(curve);
     animation.addStatusListener(listener);
     controller.forward();
   }
 
   void listener (AnimationStatus status) {
-    if(status == AnimationStatus.completed){
+    if(status == AnimationStatus.completed) {
       controller.reverse();
-    } else if(status == AnimationStatus.dismissed){
+    } else if (status == AnimationStatus.dismissed){
       controller.forward();
     }
-    //this code here animates the logo back and forth in itsd own without depending upon any external library.
-  }
-
-  Widget builder(BuildContext context , Widget child) {
-    return Container(
-      height: animation.value,
-      width: animation.value,
-      child: FlutterLogo(),
-    );
   }
 
   @override
@@ -57,12 +72,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
       ),
       body: Container(
         padding: EdgeInsets.all(32),
-        child: Center(
-          child: AnimatedBuilder(
-            animation: animation,
-            builder: builder,
-          )
-        ),
+          child: AnimatedLogo(animation: animation),
       ),
     );
   }
