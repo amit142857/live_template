23507d3ca436658665746caf3907081463320634
Amit <ydvamit82@gmail.com>
2021-05-10 23:34:51 +0545

    flutter_maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/animation.dart';
-
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -14,56 +14,7 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class AnimatedLogo extends AnimatedWidget{
-
-  static final _opacityTween = Tween<double>(begin: 0.1 , end: 1.0);
-  static final _sizeTween = Tween<double>(begin: 0.0 , end: 300.0);
-  static final _rotateTween = Tween<double>(begin: 0.0 , end: 12.0);
-
-  AnimatedLogo({Key key, Animation<double> animation}) : super (key: key, listenable: animation);
-
-  Widget build(BuildContext context){
-    final Animation<double> animation = listenable;
-    
-    return Center(
-      child: Transform.rotate(angle: _rotateTween.evaluate(animation),
-      child: Opacity(opacity: _opacityTween.evaluate(animation),
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10.0),
-        height: _sizeTween.evaluate(animation),
-        width: _sizeTween.evaluate(animation),
-        child: FlutterLogo(),
-      ),
-      ),
-      ),
-    );
-  }
-}
-
-class _HomeState extends State<Home> with TickerProviderStateMixin {
-  Animation animation;
-  AnimationController controller;
-
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller = AnimationController(duration: const Duration(milliseconds: 3000), vsync: this);
-    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
-
-    animation.addStatusListener(listener);
-    controller.forward();
-  }
-
-  void listener (AnimationStatus status) {
-    if(status == AnimationStatus.completed) {
-      controller.reverse();
-    } else if (status == AnimationStatus.dismissed){
-      controller.forward();
-    }
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,7 +23,26 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
       ),
       body: Container(
         padding: EdgeInsets.all(32),
-          child: AnimatedLogo(animation: animation),
+        child: Center(
+          child: Column(
+            children:<Widget> [
+              Flexible(
+                  child: FlutterMap(
+                    options: MapOptions(
+                      center: LatLng(41.8781, -87.6298),
+                      zoom: 5.0
+                    ),
+                    layers: [
+                      TileLayerOptions(
+                        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        subdomains: ['a' , 'b' , 'c']
+                      )
+                    ],
+                  )
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
