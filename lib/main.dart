c1167978d88c5e628c4c31e4e9a94d393f3e504b
Amit <ydvamit82@gmail.com>
2021-05-17 15:01:53 +0545

    alex and alexa

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: new MyApp(),
   ));
 }
@@ -50,15 +51,103 @@ class _State extends State<MyApp> {
       body: new Container(
           padding: new EdgeInsets.all(32.0),
           child: new Center(
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                new RaisedButton(onPressed: _showUrl, child: new Text('URL'),),
-                new RaisedButton(onPressed: _showEmail, child: new Text('Email'),),
-                new RaisedButton(onPressed: _showSms, child: new Text('Sms'),),
-                new RaisedButton(onPressed: _showTelephone, child: new Text('Telephone'),),
-
+              children: [
+                Container(
+                  width: 200,
+                  height: 160,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    image: DecorationImage(
+                      image: AssetImage('Images/amit.jpg'),
+                      fit: BoxFit.contain
+                    ),
+                      border: Border.all(color: Colors.greenAccent , width: 5.0),
+                    gradient: RadialGradient(colors: <Color>[Colors.greenAccent , Colors.green])
+                  ),
+                ),
+                Text("Amit Yadav" ,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold
+                ),
+                  textAlign: TextAlign.center,
+                ),
+                Text("Sr. Flutter Developer" ,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text("Alex Alexa Software Solutions PVT. LTD" ,
+                  style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new RaisedButton(onPressed: _showTelephone, child: Icon(Icons.call),),
+                    new RaisedButton(onPressed: _showEmail, child: Icon(Icons.mail),),
+                    new RaisedButton(onPressed: _showSms, child: Icon(Icons.mark_chat_unread),),
+                  ],
+                ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            image: DecorationImage(
+                                image: AssetImage('Images/laxmi.jpg'),
+                                fit: BoxFit.contain
+                            ),
+                            border: Border.all(color: Colors.greenAccent , width: 5.0),
+                            gradient: RadialGradient(colors: <Color>[Colors.greenAccent , Colors.green])
+                        ),
+                      ),
+                      Text("Laxmi Mahato" ,
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text("Sr. Java Developer" ,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text("Alex Alexa Software Solutions PVT. LTD" ,
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          new RaisedButton(onPressed:() => _launch('tel:981-797-3974'), child: Icon(Icons.call),),
+                          new RaisedButton(onPressed:() => _launch('mailto:laxmi.mahato.lm.9803@gmail.com') , child: Icon(Icons.mail),),
+                          new RaisedButton(onPressed:() => _launch('sms:981-797-3974'), child: Icon(Icons.mark_chat_unread),),
+                        ],
+                      ),
+                    ],
+                  ),
               ],
             ),
           )
